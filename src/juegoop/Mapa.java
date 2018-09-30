b833495d3fb124408892c8e4d0efc876ec2f2f10
nidagumo <nidagumo@hotmail.com>
2018-09-30 13:05:54 -0500

    Clases

--- src/juegoop/Mapa.java
+++ src/juegoop/Mapa.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package juegoop;
-
-/**
- *
- * @author RUTH MORALES
- */
 public class Mapa {
+    int mapa1[][]=new int [15][15];
     
 }
